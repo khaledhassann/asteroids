169b287fb1103e9b623c9f2ceb96d497dfeb3851
Khaled Hassan <62528496+khaledhassann@users.noreply.github.com>
2022-12-26 03:04:44 +0200

    Update linked_list.h

--- Asteroids_github_ASU/linked_list.h
+++ Asteroids_github_ASU/linked_list.h
@@ -1,24 +1,35 @@
+#pragma once
 #include <stdio.h>
 #include <ostream>
 #include <istream>
 #include <iostream>
-using namespace std;
-typedef int T;
+#include "sSpaceObject.h"
 
-#pragma once
+using namespace std;
+typedef sSpaceObject X;
+
+
 class LinkedList {
 
 private:
 	class Node {
 	public:
 		// ******** NODE DATA MEMBERS **********
-		T data;	// data part of the node
+		X data;	// data part of the node
 		Node* next;			// node ptr that points to the next node
-		Node() : data(0), next(0) {}
-		Node(T dataValue) : data(dataValue), next(0) {}
+		Node() : data(), next(0) {}
+		Node(X dataValue) : data(dataValue), next(0) {}
+		X& getData() {
+			return data;
+		}
+		Node* getNext() {
+			return next;
+		}
 	};
-	// ******* LINKED LIST DATA MEMBERS **********
+public:
 	typedef Node* NodePointer;
+	// ******* LINKED LIST DATA MEMBERS **********
+private:
 	NodePointer first;		// this is the first ptr that points to the first node
 	int mySize;				// mySize = size of the list
 public:
@@ -32,25 +43,26 @@ public:
 
 	void erase(int index); // erase the node at the given index
 
-	int search(T dataVal);	// searches for a specific value
+	//int search(T dataVal);	// searches for a specific value
 
 	//void insertFirst(ElementType newItem);		// this inserts an item into index 0 of the list
 
-	void insert(T dataVal, int index); // inserting in any given index
+	void insert(X dataVal, int index); // inserting in any given index
 
-	void push_back(T dataVal); // inserts element at last position of the list
+	void push_back(X dataVal); // inserts element at last position of the list
 
 	const LinkedList& operator = (const LinkedList& rightSide); // overriding the = operator
 
-	void display(ostream& out) const;		// this traverses over the Linked Lists and couts it's elements
+	//void display(ostream& out) const;		// this traverses over the Linked Lists and couts it's elements
 
-	const LinkedList operator + (const LinkedList& rightSide); // overloading the + operator
+	//const LinkedList operator + (const LinkedList& rightSide); // overloading the + operator
 
 	bool operator > (const LinkedList& rightSide); // overloading the > (greater than operator)
-
+	int getSize();
+	NodePointer getFirst();
 };
 
-ostream& operator<< (ostream& out, const LinkedList& aList);	// this overloads the output operator
+/*ostream& operator<< (ostream& out, const LinkedList& aList);*/	// this overloads the output operator
 
-istream& operator >> (istream& in, LinkedList& aList); // overloading the input operator
+//istream& operator >> (istream& in, LinkedList& aList); // overloading the input operator
 
